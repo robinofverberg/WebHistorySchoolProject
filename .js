e854877388c4317eb619b52bca921ebff96c90db
robinofverberg <robin.ofverberg@gmail.com>
2021-10-01 19:20:31 +0200

    Uppdat 2
    + JS
    +Sticky header
    +scrollsida
    +nybackgrund
    +annanlänk-text

---
+++ .js
@@ -0,0 +1,16 @@
+//window.onscroll = function() {myStickyFunction()};
+
+
+//var header = document.getElementById("myHeader");
+
+
+//var sticky = header.offsetTop;
+
+
+//function myStickyFunction() {
+  //if (window.pageYOffset > sticky) {
+    //header.classList.add("sticky");
+  //} else {
+    //header.classList.remove("sticky");
+  //}
+//} 
