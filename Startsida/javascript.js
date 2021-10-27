c47b3886a004cc00f7e5d51a8c4244174617751d
robinofverberg <robin.ofverberg@gmail.com>
2021-10-27 21:00:32 +0200

    INTRO TILL STARTSIDA

--- Startsida/javascript.js
+++ Startsida/javascript.js
@@ -2,3 +2,37 @@ function hamburgerAnimation(x) {
   x.classList.toggle("change");
   document.getElementById ("hamburgerMenu").classList.toggle("hamburgerMenuHidden")
 }
+
+let intro = document.querySelector('.intro');
+let logo = document.querySelector('.logo-header');
+let logoSpan = document.querySelectorAll('.logo')
+
+window.addEventListener('DOMContentLoaded', ()=>{
+
+  setTimeout(() =>{
+
+    logoSpan.forEach((span, idx) =>{
+
+      setTimeout(()=>{
+        span.classList.add('active')
+      },(idx + 1) * 400)
+    });
+
+      setTimeout(()=>{
+        logoSpan.forEach((span, idx)=>{
+
+          setTimeout(()=>{
+            span.classList.remove('active')
+            span.classList.add('fade')
+          }, (idx + 1) * 50)
+
+        })
+      }, 2000)
+
+      setTimeout(()=>{
+
+        intro.style.top = '-100vh'
+      },2400)
+    
+  })
+})
